7fcbab9d5083ea17f594e8b780a192182a56927f
santosdeivissonsilva <santos.deivissonsilva@hotmail.com>
2023-05-13 13:01:01 -0300

    fist_commit

---
+++ fakepinterest/__init__.py
@@ -0,0 +1,17 @@
+# Criação da aplicação
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SECRET_KEY"] = "390b8ff59fe0cff7a01892ac7d0b5632"
+app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "homepage"
+
+from fakepinterest import routes
